8c749aaac9ff5b117811c723c5da29b227c685e3
spongessuck <spongessuck@gmail.com>
2015-02-17 16:17:27 -0500

    Create gm.waitsFor.js

---
+++ gm.waitsFor.js
@@ -0,0 +1,38 @@
+(function() {
+  angular.module('gm.waitsFor', [])
+  .directive('waitsFor', function() {
+		var defaultTemplateUrl = 'spinner.html';
+		var defaultTemplate = '<span>Loading...</span>';
+		return {
+			restrict:'A',
+			controller:['$scope', '$templateCache', function($scope, $templateCache) {
+				defaultTemplate = $templateCache.get(defaultTemplateUrl) || defaultTemplate;
+			}],
+			link:function(scope, elem, attrs) {
+				var contents = elem.contents();
+				contents.addClass('hidden');
+				
+				var msgEl;
+				
+				var waitingTemplate = attrs.waitingTemplate || defaultTemplate;
+				
+				msgEl = angular.element(waitingTemplate);
+				
+				if(attrs.waitingMessage)
+					msgEl.append('<span>' + attrs.waitingMessage + '</span>');
+				
+				elem.append(msgEl);
+				
+				var cancelWatch = scope.$watch(function() {
+					return scope.$eval(attrs.waitsFor);
+				}, function(newVal) {
+					if(newVal) {
+						cancelWatch();
+						contents.removeClass('hidden');
+						msgEl.remove();
+					}
+				});
+			}
+		}
+	});
+})();
