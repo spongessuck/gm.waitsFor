25b8c20335751aebb4dccf3b270c6740581914b0
Greg McGee <spongessuck@gmail.com>
2016-04-15 14:50:35 -0400

    Update config, bump version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
 module.exports = function(grunt) {
-	
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		
+
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -12,23 +12,23 @@ module.exports = function(grunt) {
 				dest: 'dist/gm.waitsFor.min.js'
 			}
 		},
-		
+
 		copy: {
 			def: {
 				files: [{
 					'dist/gm.waitsFor.js':'src/gm.waitsFor.js'
 				}]
-			},
+			}
 		},
-		
+
 		watch: {
 			files: ['src/**'],
 			tasks: ['uglify']
 		}
 	});
-	
+
 	//grunt.loadNpmTasks('grunt-contrib-watch');
-	
+
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
